cf7447fe0294c9a610764efee826fbb4f7317707
Harshita gupta <84032318+harshitagupta934@users.noreply.github.com>
2024-04-25 13:57:21 +0530

    program1.js

--- program1.js
+++ program1.js
@@ -1,7 +1,22 @@
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    let start = 0;
+    let charIndexMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charIndexMap[char] >= start) {
+            start = charIndexMap[char] + 1;
+        }
+        charIndexMap[char] = i;
+        maxLength = Math.max(maxLength, i - start + 1);
+    }
+
+    return maxLength;
 }
 
 module.exports = { longestSubstring };
 
+// Test cases
+console.log(longestSubstring("abcabcbb")); // Output: 3
 
